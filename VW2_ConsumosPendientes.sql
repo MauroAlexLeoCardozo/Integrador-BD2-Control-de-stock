a9ca012f139714ce0e255f6ce1ca62c2dc581ebd
MauroAlexLeoCardozo <mauro.cardozo.dev@outlook.com>
2026-07-18 01:14:39 -0300

    Segunda vista, muestran los pendientes a consumir

---
+++ VW2_ConsumosPendientes.sql
@@ -0,0 +1,7 @@
+create view VW_ConsumosPendientes as
+select p.Nombre "Nombre y Apellido", i.Nombre, c.Cantidad, c.Fecha
+from Consumo c
+inner join Insumo i on c.IDInsumo = i.ID
+inner join Empleado e on c.IDSolicitante = e.ID
+inner join Persona p on e.IDPersona = p.ID
+where c.IDAprobador is null and c.Estado = 1
